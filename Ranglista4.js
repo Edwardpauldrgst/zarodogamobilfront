271f9dc6948ce5f954f97ba7376eab591424a069
Edwardpauldrgst <82730144+Edwardpauldrgst@users.noreply.github.com>
2022-04-25 01:22:51 +0200

    vegleges

--- Ranglista4.js
+++ Ranglista4.js
@@ -15,7 +15,7 @@ export default class FetchExample extends React.Component {
       bevitel1:szam
     }
 
-  fetch("http://172.16.0.180:3000/Ranglista4", {
+  fetch("http://localhost:3000/Ranglista4", {
       method: "POST",
       body: JSON.stringify(bemenet),
       headers: {"Content-type": "application/json; charset=UTF-8"}
@@ -29,7 +29,7 @@ export default class FetchExample extends React.Component {
 
 
   componentDidMount(){
-    return fetch('http://172.16.0.180:3000/Ranglista4')
+    return fetch('http://localhost:3000/Ranglista4')
       .then((response) => response.json())
       .then((responseJson) => {
 
