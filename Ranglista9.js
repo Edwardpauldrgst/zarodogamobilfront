48516f333fa76422909a653449bc68adfa37fd6d
Edwardpauldrgst <82730144+Edwardpauldrgst@users.noreply.github.com>
2022-04-25 01:24:16 +0200

    localhost

--- Ranglista9.js
+++ Ranglista9.js
@@ -15,7 +15,7 @@ export default class FetchExample extends React.Component {
       bevitel1:szam
     }
 
-  fetch("http://172.16.0.180:3000/Ranglista9", {
+  fetch("http://localhost:3000/Ranglista9", {
       method: "POST",
       body: JSON.stringify(bemenet),
       headers: {"Content-type": "application/json; charset=UTF-8"}
@@ -29,7 +29,7 @@ export default class FetchExample extends React.Component {
 
 
   componentDidMount(){
-    return fetch('http://172.16.0.180:3000/Ranglista9')
+    return fetch('http://localhost:3000/Ranglista9')
       .then((response) => response.json())
       .then((responseJson) => {
 
